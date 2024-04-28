4967c5271a7515cb8a37f36dd0fc05e9902840cc
TaiFilipelli <taielfilipelli@gmail.com>
2024-04-28 17:43:22 -0300

    multiple themes, spring and wouter

---
+++ src/components/login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './login.css';
+
+const Login = () => {
+  return (
+    <div className='inicio'>
+      <img className='logo' src='../assets/logo-eclipse.png' />
+      <h1>Bienvenido a Clipse</h1>
+    </div>
+  )
+}
+
+export default Login
