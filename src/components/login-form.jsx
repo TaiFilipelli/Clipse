5607435fbda9e9b4941bbf79c46835633594ce6a
TaiFilipelli <taielfilipelli@gmail.com>
2024-04-30 20:19:27 -0300

    animations with react-spring. Looking beautiful.

--- src/components/login-form.jsx
+++ src/components/login-form.jsx
@@ -1,10 +1,24 @@
 import React, { useState } from 'react'
 import './login-form.css';
 import { Link } from 'wouter';
+import { useSpring, animated } from '@react-spring/web';
 
 const LoginForm = () => {
     const [user, setUser] = useState('');
     const [password, setPassword] = useState('');
+    const [showButton, setShowButton] = useState(true);
+
+    const [springs,api] = useSpring(()=>({
+      from:{y:500},
+    }));
+
+    const handleClickShowForm=()=>{
+      api.start({
+        from:{y:500},
+        to:{y:0},
+      })
+      setShowButton(false);
+    }
 
     const handleUserChange= (e) =>{
         setUser(e.target.value);
@@ -22,7 +36,8 @@ const LoginForm = () => {
     }
   return (
     <div>
-      <section className="login">
+      {showButton &&(<button type='submit' className='links' onClick={handleClickShowForm}>Ingresar</button>)}
+      <animated.section className="login" style={{...springs,}}>
         <label htmlFor="user">Nombre de usuario</label>
         <br />
         <input type="text" placeholder='Usuario' value={user} onChange={handleUserChange}/>
@@ -32,7 +47,7 @@ const LoginForm = () => {
         <input type="password" placeholder='Contraseña' value={password} onChange={handlePasswordChange}/><br />
         <button type='submit' className='links'>Iniciar sesión</button><br />
         <Link to='/registro' className='links'>Quiero crear mi usuario</Link>
-      </section>
+      </animated.section>
     </div>
   )
 }
